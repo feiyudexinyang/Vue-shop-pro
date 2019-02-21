56c9f9fda3dfa021c387ac9b4015723a2ff92fda
jixieshi <653250514@qq.com>
2019-02-22 01:16:44 +0800

    后台首页面主结构创建完毕

--- src/main.js
+++ src/main.js
@@ -15,7 +15,19 @@ import ElementUI from 'element-ui'
 import axios from 'axios'
 // axios请求根地址
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
-// 把axios配置给VUE的$http成员
+// axios的请求拦截(在其中配置token)
+axios.interceptors.request.use(
+  function(config) {
+    // config:代表axios的子级配置对象
+    var token = window.sessionStorage.getItem('token')
+    config.headers.Authorization = token
+    return config
+  },
+  function(error) {
+    return Promise.reject(error)
+  }
+)
+// 给axios配置给VUE的$http成员
 Vue.prototype.$http = axios
 
 // 把element-ui注册给Vue
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,31 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
 import Home from '@/components/Home'
+import Welcome from '@/components/Welcome'
 Vue.use(Router)
 
-export default new Router({
+var router = new Router({
   routes: [
-    { path: '/home', component: Home },
-    { path: '/login', component: Login }
+    { path: '/login', component: Login },
+    { path: '/home', component: Home,redirect:'/Welcome',children:[
+      {path:'/Welcome',component:Welcome}
+    ]},
   ]
 })
+
+// 给路由设置"导航守卫"
+// 在守卫中对token进行监听,有token就让执行,否则就跳转到登录页去
+router.beforeEach((to, from, next) => {
+  // 请求login直接通过
+  if (to.path === '/login') {
+    return next()
+  }
+  // 请求非login,就判断token
+  var token = window.sessionStorage.getItem('token')
+  if (!token) {
+    return next('/login')
+  }
+  next() // 请继续往下走
+})
+
+export default router
